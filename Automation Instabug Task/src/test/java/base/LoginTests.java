581120584d070951c9349de071b710f91b40eb8b
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2024-02-07 20:19:55 +0200

    added documentation

--- Automation Instabug Task/src/test/java/base/LoginTests.java
+++ Automation Instabug Task/src/test/java/base/LoginTests.java
@@ -1,18 +1,21 @@
 package base;
 
-
 import org.testng.annotations.Test;
-import pages.LoginPage;
 import pages.ProductsPage;
 
 import static org.testng.Assert.*;
 
+/**
+ * Test class for validating login functionality of the application.
+ * Inherits common setup and teardown methods from the Base class.
+ */
 public class LoginTests extends base{
 
 
     @Test(priority = 8)
     public void testSuccessfullLogin(){
- 
+
+        // Set username and password
         loginPage.setUsername("standard_user");
         loginPage.setPassword("secret_sauce");
 
@@ -22,16 +25,16 @@ public class LoginTests extends base{
         assertTrue(productsPage.pageSecondaryTitle.getText().equals("Products"),"User is logged in");
     }
 
-    @Test(priority=1)
+    @Test(priority=7)
     public void testInvalidUsernameAndInvalidPassLogin(){
- 
+
         loginPage.setUsername("asd");
         loginPage.setPassword("123");
         loginPage.loginBtn.click();
         assertTrue(loginPage.readErrorText().contains("Username and password do not match any user in this service"),
                 "Error message for invalid username and password is correct");
     }
-    @Test(priority=5)
+    @Test(priority=1)
     public void testEmptyUsernameAndEmptyPassLogin(){
 
         loginPage.setUsername("");
@@ -40,27 +43,28 @@ public class LoginTests extends base{
         assertTrue(loginPage.readErrorText().contains("Username is required"),
                 "Error message for invalid username and password is correct");
     }
-    @Test(priority=6)
-    public void testValidUsernameAndEmptyPassLogin(){
+    @Test(priority=3)
+    public void testFilledUsernameAndEmptyPassLogin(){
 
-        loginPage.setUsername("standard_user");
+        loginPage.setUsername("asd");
         loginPage.setPassword("");
         loginPage.loginBtn.click();
         assertTrue(loginPage.readErrorText().contains("Password is required"),
                 "Error message for invalid username and password is correct");
     }
-    @Test(priority=7)
+    @Test(priority=2)
     public void testEmptyUsernameAndValidPassLogin(){
 
-        loginPage.setUsername("");
         loginPage.setPassword("secret_sauce");
+        loginPage.setUsername("");
         loginPage.loginBtn.click();
         assertTrue(loginPage.readErrorText().contains("Username is required"),
                 "Error message for invalid username and password is correct");
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void testInvalidUsernameAndValidPassLogin(){
+
         loginPage.setUsername("asd");
         loginPage.setPassword("secret_sauce");
         loginPage.loginBtn.click();
@@ -68,7 +72,7 @@ public class LoginTests extends base{
                 "Error message for invalid username and password is correct");
     }
 
-    @Test(priority=2)
+    @Test(priority=5)
     public void testValidUsernameAndInvalidPassLogin(){
         loginPage.setUsername("standard_user");
         loginPage.setPassword("123");
@@ -77,7 +81,7 @@ public class LoginTests extends base{
                 "Error message for invalid username and password is correct");
     }
 
-    @Test(priority=4)
+    @Test(priority=6)
     public void testLockedoutUser(){
         loginPage.setUsername("locked_out_user");
         loginPage.setPassword("secret_sauce");
--- Automation Instabug Task/src/test/java/base/base.java
+++ Automation Instabug Task/src/test/java/base/base.java
@@ -7,28 +7,42 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.LoginPage;
 
-
+/**
+ * Base class for Selenium test automation framework.
+ * Responsible for setting up and tearing down the WebDriver instance.
+ */
 public class base {
 
-    protected WebDriver driver;
+    protected WebDriver driver; // WebDriver instance to interact with the browser
 
-    protected LoginPage loginPage;
+    protected LoginPage loginPage; // Object of the LoginPage class to access login page elements
 
+    /**
+     * Method to set up the WebDriver instance before executing tests.
+     * Initializes WebDriver, navigates to the application URL, and maximizes the browser window.
+     */
     @BeforeClass
     public void setup(){
 
+        // Setup WebDriver using WebDriverManager for Chrome
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+
+        // Navigate to the application URL and maximize the window
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
 
+        // Initialize LoginPage object with WebDriver instance
         loginPage = new LoginPage(driver);
-
     }
 
+    /**
+     * Method to tear down the WebDriver instance after executing tests.
+     * Quits the WebDriver session.
+     */
     @AfterClass
     public void tearDown(){
+        // Quit the WebDriver session
         driver.quit();
     }
-
 }
